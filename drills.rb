65ab83094b4a2c42b26829ae69b81b27240c88ab
Ted Anyansi <tanyansi@gmail.com>
2016-08-06 22:06:04 -0700

    finished first set on arguments

--- drills.rb
+++ drills.rb
@@ -4,24 +4,56 @@
 #say_hello
   # returns 'hello'
 
+def say_hello()
+
+"hello"
+
+end
+
 #echo
   # returns the input string
 
+def echo(string)
+string
+end
+
 #eddie_izzards_height
-  # calculates and returns Eddie Izzard's height 
+  # calculates and returns Eddie Izzard's height
   # takes in the height of heels he's wearing (default value: 0)
   # and adds heel height to his base 67 inches for the result
+
+def eddie_izzards_height (heels_height = 0)
+
+  heels_height + 67
+
+end
 
 #how_many_args
   # accepts any number of arguments without complaint
   # returns the total number of arguments passed to the function
   # Hint: google ruby *args
 
-#find_answer 
+def how_many_args (*parameters)
+  parameters.length
+end
+
+
+#find_answer
   # returns the value associated with the 'answer' keyword argument
   # returns nil if it cannot find the 'answer' keyword argument
   # complains when given non-keyword arguments
   # Hint: google ruby keyword arguments
+
+# def find_answer(answer: nil)
+#  answer
+#
+# end
+
+def find_answer(kwargs={})
+  kwargs[:answer]
+end
+
+
 
 ##############################
 #### MANIPULATING STRINGS ####
